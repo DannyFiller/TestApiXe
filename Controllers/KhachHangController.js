09cca36fa93a2fbaf3de3c5abebcf42b51492da2
quanHoang <quanhoang28022003@gmail.com>
2023-11-21 12:37:12 +0700

    Sửa khách hàng

--- Controllers/KhachHangController.js
+++ Controllers/KhachHangController.js
@@ -18,6 +18,14 @@ const KhachHangController={
             res.status(500).json(err);
         }
     },
-    
+    updateKhachHang:async(req,res)=>{
+        try{
+            const kh=await KhachHang.findById(req.params.id);
+            await kh.updateOne({$set:req.body});
+            res.status(200).json("updateSuccess");
+        }catch(err){
+            res.status(500).json(err);
+        }    
+    },
 }
 module.exports=KhachHangController;
--- Routes/KhachHang.js
+++ Routes/KhachHang.js
@@ -2,6 +2,7 @@ const KhachHangController=require("../Controllers/KhachHangController")
 const router=require("express").Router();
 
 router.post("/",KhachHangController.addKhachHang);
+router.put("/:id",KhachHangController.updateKhachHang);
 router.get("/",KhachHangController.getAllKhachHang);
 
 module.exports=router;
