3f45fe8792bb906f0ab6f928f381af8d836ee0ca
DannyFillerSD <tuanvakhoi2991@gmail.com>
2024-07-02 20:55:49 +0700

    .

--- Controllers/PhanLoaiController.js
+++ Controllers/PhanLoaiController.js
@@ -11,46 +11,47 @@ const PhanLoaiController = {
         }
     },
     
-    addPhanLoai: async(req, res) => {
-        try{
-            const newPhanLoai = new PhanLoai(req.body);  
-            const savePhanLoai= await newPhanLoai.save();
-            res.status(200).json(savePhanLoai);
-        }catch(err){
-            res.status(500).json(err);
-        }
-    },
+//     addPhanLoai: async(req, res) => {
+//         try{
+//             const newPhanLoai = new PhanLoai(req.body);  
+//             const savePhanLoai= await newPhanLoai.save();
+//             res.status(200).json(savePhanLoai);
+//         }catch(err){
+//             res.status(500).json(err);
+//         }
+//     },
 
-    updatePhanLoai:async(req,res) => {
-        try{
-            const phanLoaiUpdate = await PhanLoai.findById(req.params.id);
-            await phanLoaiUpdate.updateOne({$set:req.body});
-            res.status(200).json(phanLoaiUpdate);
-        }catch(err){
-            res.status(500).json(err);
-        }
-    },
+//     updatePhanLoai:async(req,res) => {
+//         try{
+//             const phanLoaiUpdate = await PhanLoai.findById(req.params.id);
+//             await phanLoaiUpdate.updateOne({$set:req.body});
+//             res.status(200).json(phanLoaiUpdate);
+//         }catch(err){
+//             res.status(500).json(err);
+//         }
+//     },
     
     
-    deletePhanLoai:async(req,res) =>{
-        try{
-            const phanLoaiDel = await PhanLoai.findByIdAndDelete(req.params.id);
-            res.status(200).json(phanLoaiDel);
-        }catch(err){
-            res.status(500).json(err);
-        }
-    },
+//     deletePhanLoai:async(req,res) =>{
+//         try{
+//             const phanLoaiDel = await PhanLoai.findByIdAndDelete(req.params.id);
+//             res.status(200).json(phanLoaiDel);
+//         }catch(err){
+//             res.status(500).json(err);
+//         }
+//     },
 
 
-    getPhanLoai1:async(req,res) =>{
-        try{
-            const phanLoais = await PhanLoai.find({MaPhanLoai: req.params.MaPL});
-            res.status(200).json(phanLoais);
-        }catch(err){
-            res.status(500).json(err);
-        }
-    }
-  };
+//     getPhanLoai1:async(req,res) =>{
+//         try{
+//             const phanLoais = await PhanLoai.find({MaPhanLoai: req.params.MaPL});
+//             res.status(200).json(phanLoais);
+//         }catch(err){
+//             res.status(500).json(err);
+//         }
+//     }
+//   };
+}
   
   module.exports = PhanLoaiController;
   
